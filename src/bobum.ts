8cc85d27942f3aa23129682490f892945086c16b
Timofey Peshin <timoffex@gmail.com>
2022-08-21 23:34:40 -0700

    Add multiple moving boba

---
+++ src/bobum.ts
@@ -0,0 +1,73 @@
+/** A bobum is the singular of boba. */
+export class Bobum {
+    private _position = { x: 0, y: 0 };
+    private _velocity = { x: 0, y: 0 };
+    private _radius = 20;
+
+    private _targetCenter = { x: 0, y: 0 };
+    private _target = { x: 0, y: 0 };
+
+    private _isDead = false;
+
+    static newAt(position: { x: number, y: number }): Bobum {
+        const bobum = new Bobum();
+        bobum._position = structuredClone(position);
+        bobum._targetCenter = structuredClone(position);
+        bobum._target = structuredClone(position);
+        return bobum;
+    }
+
+    get position() {
+        return structuredClone(this._position);
+    }
+
+    get radius() {
+        return this._radius;
+    }
+
+    updateTargetCenter(x: number, y: number): void {
+        this._targetCenter.x = x;
+        this._targetCenter.y = y;
+    }
+
+    updatePosition(msDelta: number): void {
+        const secondsDelta = msDelta / 1000.0;
+        const acceleration = 10;
+
+        let dirX = this._target.x - this._position.x;
+        let dirY = this._target.y - this._position.y;
+        const dirMagnitude = Math.sqrt(dirX * dirX + dirY * dirY);
+        if (dirMagnitude > 0) {
+            dirX /= dirMagnitude;
+            dirY /= dirMagnitude;
+
+            this._velocity.x += dirX * acceleration * secondsDelta;
+            this._velocity.y += dirY * acceleration * secondsDelta;
+        }
+
+        this._velocity.x *= 0.99;
+        this._velocity.y *= 0.99;
+
+        this._position.x += this._velocity.x * secondsDelta;
+        this._position.y += this._velocity.y * secondsDelta;
+    }
+
+    start(): void {
+        this.updateTargetAndRepeat();
+    }
+
+    kill(): void {
+        this._isDead = true;
+    }
+
+    private updateTargetAndRepeat() {
+        this._target.x = this._targetCenter.x + (Math.random() * 2 - 1) * 50;
+        this._target.y = this._targetCenter.y + (Math.random() * 2 - 1) * 50;
+
+        if (!this._isDead) {
+            setTimeout(() => this.updateTargetAndRepeat(), 500 + 1000 * Math.random());
+        }
+    }
+
+    private constructor() { }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,19 +2,7 @@ import './video_overlay.scss';
 
 import { CanvasManager } from './canvas-manager';
 import { TeaPainter } from './tea-painter';
-
-const circle = {
-    x: 400,
-    y: 400,
-    vx: 0,
-    vy: 0,
-    radius: 20
-};
-
-const target = {
-    x: 400,
-    y: 400,
-};
+import { Bobum } from './bobum';
 
 const teaPainter = new TeaPainter(
     /*amplitude=*/10,
@@ -23,60 +11,67 @@ const teaPainter = new TeaPainter(
     /*stepSize=*/3,
     /*pathHeight=*/100);
 
+let boba: Bobum[] = [];
+
+let lastDrawTime = 0;
+
+window.addEventListener('resize', positionBoba);
+function positionBoba() {
+    if (!canvasManager) return;
+
+    const step = canvasManager.size.width / (boba.length + 1);
+    let x = step;
+
+    for (const bobum of boba) {
+        bobum.updateTargetCenter(x, canvasManager.size.height - 70);
+        x += step;
+    }
+}
+
 const canvasManager = CanvasManager.tryCreate();
 canvasManager?.beginPainting({
     paint: (canvas, ctx) => {
-        moveTowardTarget();
 
         ctx.clearRect(0, 0, canvas.width, canvas.height);
 
         teaPainter.paint(canvas, ctx);
 
-        ctx.beginPath();
-        ctx.arc(
-            circle.x,
-            circle.y,
-            circle.radius,
-            0, 2 * Math.PI);
+        if (boba.length == 0) {
+            console.log('Creating new boba');
 
-        ctx.fillStyle = '#B28137';
-        ctx.fill();
-        ctx.stroke();
+            boba = [
+                Bobum.newAt({x: 100, y: canvas.height - 100}),
+                Bobum.newAt({x: 200, y: canvas.height - 90}),
+                Bobum.newAt({x: 250, y: canvas.height - 110}),
+                Bobum.newAt({x: 400, y: canvas.height - 70}),
+            ];
+
+            positionBoba();
+
+            for (const bobum of boba) {
+                bobum.start();
+            }
+
+            lastDrawTime = Date.now();
+        }
+
+        const now = Date.now();
+        const msDelta = now - lastDrawTime;
+        lastDrawTime = now;
+
+        for (const bobum of boba) {
+            bobum.updatePosition(msDelta);
+
+            ctx.beginPath();
+            ctx.arc(
+                bobum.position.x,
+                bobum.position.y,
+                bobum.radius,
+                0, 2 * Math.PI);
+
+            ctx.fillStyle = '#B28137';
+            ctx.fill();
+            ctx.stroke();
+        }
     }
 });
-
-function updateTargetAndRepeat() {
-    const size = canvasManager?.size;
-    if (size) {
-        target.x = size.width / 2 + (Math.random() * 2 - 1) * 100;
-        target.y = size.height / 2 + (Math.random() * 2 - 1) * 100;
-    }
-
-    setTimeout(updateTargetAndRepeat, 500 + 1000 * Math.random());
-}
-updateTargetAndRepeat();
-
-function moveTowardTarget() {
-    const rate = 0.04;
-
-    let dirX = target.x - circle.x;
-    let dirY = target.y - circle.y;
-    const dirMagnitude = Math.sqrt(dirX * dirX + dirY * dirY);
-    if (dirMagnitude > 0) {
-        dirX /= dirMagnitude;
-        dirY /= dirMagnitude;
-    }
-
-    circle.vx += dirX * rate;
-    circle.vy += dirY * rate;
-
-    const speed2 = circle.vx * circle.vx + circle.vy * circle.vy;
-    if (speed2 > 4) {
-        const speed = Math.sqrt(speed2);
-        circle.vx /= speed;
-        circle.vy /= speed;
-    }
-
-    circle.x += circle.vx;
-    circle.y += circle.vy;
-}
