37e6c4c965768a880bafbf735f8a5b73dbd9d1fc
Timofey Peshin <timoffex@gmail.com>
2022-08-25 18:47:46 -0700

    Implement oauth-redirect-page

--- oauth-redirect-page/src/main.ts
+++ oauth-redirect-page/src/main.ts
@@ -1,5 +1,20 @@
 const token = document.location.hash;
 
-const paragraph = document.createElement('p');
-paragraph.innerHTML = `Received token: ${token}`;
-document.body.appendChild(paragraph);
+if (window.opener) {
+    const params = new Map(token.slice(1).split('&').map((param) => {
+        const [name, value] = param.split('=');
+        return [name, value];
+    }));
+
+    const message = JSON.stringify({
+        access_token: params.get('access_token'),
+        scope: params.get('scope'),
+    });
+
+    console.log(`Found opener. Posting: ${message}`);
+    window.opener.postMessage(message, '*');
+} else {
+    console.log('No opener found.');
+}
+
+window.close();
