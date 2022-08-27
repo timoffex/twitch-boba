11b53da4bf0a3d40c1a7272ea9544110cb7dd78f
Timofey Peshin <timoffex@gmail.com>
2022-08-27 15:31:18 -0700

    Serve 'overlay' from localhost in overlay-server

--- overlay-server/webpack.main.config.js
+++ overlay-server/webpack.main.config.js
@@ -1,3 +1,6 @@
+const CopyFilesPlugin = require('copy-webpack-plugin');
+const path = require('path');
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
@@ -11,4 +14,14 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css', '.json'],
   },
+  plugins: [
+    new CopyFilesPlugin({
+      patterns: [
+        {
+          from: path.join(path.dirname(require.resolve('@twitch-boba/overlay/package.json')), 'dist/'),
+          to: './public/overlay/'
+        }
+      ]
+    })
+  ]
 };
--- overlay/webpack.config.js
+++ overlay/webpack.config.js
@@ -25,6 +25,7 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+        publicPath: '/public/overlay/',
     },
     externals: [nodeExternals()],
 }
